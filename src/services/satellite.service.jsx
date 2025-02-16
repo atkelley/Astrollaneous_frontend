cf15db7c3b3cd69e662463b35013e11158f5923f
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2025-02-16 14:37:13 -0500

    Issue with Satellites API url

--- src/services/satellite.service.jsx
+++ src/services/satellite.service.jsx
@@ -1,4 +1,4 @@
 import { api } from "../api/api";
 
-export const getSatellites = () => api.get(`/satellites/`); 
-export const getSatellite = (name) => api.get(`/satellites/${name}`); 
+export const getSatellites = () => api.get(`/space/satellites`); 
+export const getSatellite = (name) => api.get(`/space/satellites/${name}`); 
